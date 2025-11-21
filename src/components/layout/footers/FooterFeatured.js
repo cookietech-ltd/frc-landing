bd5328b7dbfaf533f24dc83799e4dd22df1b612c
Soumik205 <soumikghosh205@gmail.com>
2025-11-21 21:51:42 +0600

    fix: footer UI

--- src/components/layout/footers/FooterFeatured.js
+++ src/components/layout/footers/FooterFeatured.js
@@ -20,7 +20,7 @@ const FooterFeatured = () => {
     },
     {
       title: "Office Address",
-      subTitle: "721 Aunt Clara Rd, Burgettstown Pa 15021",
+      subTitle: "105 international drive Oakdale PA 15071",
       img: contactImage9,
       path: "/locations",
     },
@@ -28,20 +28,26 @@ const FooterFeatured = () => {
   return (
     <div className="contact-feature-area before-bg-bottom mb--30">
       <div className="container">
-        <div className="row justify-content-center">
-          {contact?.map(({ title, subTitle, img, path }, idx) => (
-            <div key={idx} className="col-lg-4 col-md-6 col-12">
-              <div className="ltn__contact-feature-item">
-                <div className="ltn__contact-feature-icon">
-                  <Image src={img} alt="Icon Image" />
+        <div className="row justify-content-center align-items-strech">
+          {contact.map(({ title, subTitle, img, path }, idx) => (
+            <div key={idx} className="col-lg-4 col-md-6 col-12 d-flex">
+              <div className="ltn__contact-feature-item text-center d-flex flex-row justify-content-center flex-grow-1">
+                <div className="ltn__contact-feature-icon "> {/*mb-3*/}
+                  <Image src={img} alt="Icon Image" width={200} height={200} />
                 </div>
                 <div className="ltn__contact-feature-info">
-                  <Link href={path}>
+                   <Link href={path}>
                     <span className="h6">{subTitle}</span>
-                    <br />
-                    <span className="h2">{title}</span>
+                     <br />
+                     <span className="h2">{title}</span>
                   </Link>
                 </div>
+                {/* <div className="ltn__contact-feature-info">
+                  <Link href={path} className="ltn__contact-feature-link">
+                    <div className="h6">{subTitle}</div>
+                    <div className="h2">{title}</div>
+                  </Link>
+                </div> */}
               </div>
             </div>
           ))}
